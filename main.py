55e40759938932a460221494a204f1a97cbc51c0
Sunil Kumar <sdkumar@hmc.edu>
2024-01-22 13:37:14 -0800

    set up .env

--- main.py
+++ main.py
@@ -3,15 +3,27 @@
 # This program will use Groundlight's Python SDK to answer a simple question: "Is the door open?"
 
 # our Python SDK
-from groundlight import Groundlight
+import os
+from time import sleep
+
+from dotenv import load_dotenv
 
 # framegrab is our open-source Python library, which makes it easy to set up cameras for computer vision applications, like Groundlight.
 from framegrab import FrameGrabber
-from time import sleep
+from groundlight import Groundlight
+
+# loads the contents of the .env file into the environment
+load_dotenv()
+
+# Check if GROUNDLIGHT_API_TOKEN is set in the .env file and starts with "api_", if not, raise an error.
+api_token = os.getenv("GROUNDLIGHT_API_TOKEN")
+if not api_token or not api_token.startswith("api_"):
+    raise ValueError(
+        "The 'GROUNDLIGHT_API_TOKEN' is not set correctly in the .env file. It should start with 'api_'. You can create a token here: https://app.groundlight.ai/reef/my-account/api-tokens."
+    )
 
 # Initialize the SDK. We will use the gl object to interact with the Groundlight API.
 gl = Groundlight()
-
 
 # Question you want Groundlight to answer. Consider reading our short article on how to get the best results from Groundlight, which includes how to phrase your query: https://code.groundlight.ai/python-sdk/blog/best-practices
 query_text = "Is the door open? This includes if the door is only partially open."
@@ -22,7 +34,7 @@ detector = gl.get_or_create_detector(
     query=query_text,
 )
 
-
+# TODO: move to yaml
 # We will use a USB webcam for this example. Visit the framegrab repo for more information on using other types of cameras: https://github.com/groundlight/framegrab
 camera_config = {
     "input_type": "generic_usb",
@@ -33,12 +45,16 @@ camera = FrameGrabber.create_grabber(camera_config)
 while True:
     image = camera.grab()
 
-    # submit the image to Groundlight. This will return a Groundlight ImageQuery object, which will contain the answer to our question.
+    # submit the image to Groundlight. This will return a Groundlight ImageQuery object, which will contain the answer to our query.
     image_query = gl.ask_confident(detector=detector, image=image)
     print(f"The answer is {image_query.result}")
 
     # how long to wait, in seconds, between queries to Groundlight
     sleep(10)
 
+# TODO: put in a try finally block?
 # When you are done, you can release the camera, otherwise other programs will not be able to use it.
 camera.release()
+
+# TODO: add .env file
+# TODO:
