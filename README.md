40820d770f7696eac9fba6af8352b15440a2f61b
senorkumar <sdkumar@hmc.edu>
2024-01-24 17:46:49 -0800

    simplifications based on brandon's feedback

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Welcome! This is a small "Hello World" example for Groundlight. Please clone thi
 This example application uses Groundlight to answer the question: "Is the door open?". The application takes a picture from a camera connected to your computer every 10 seconds, and sends it to Groundlight for processing. 
 
 1. Create a Groundlight account at [https://app.groundlight.ai/](https://app.groundlight.ai/). 
-2. Create an API token by clicking on your account name in the top right corner, and then clicking on "API Tokens". Alternatively, you can click [here](https://app.groundlight.ai/reef/my-account/api-tokens).
+2. Create an API token by clicking on your account name in the top right corner, and then clicking on "API Tokens". Alternatively, you can click [here](https://app.groundlight.ai/reef/my-account/api-tokens) to go to the page directly.
 3. Paste your API token into the `GROUNDLIGHT_API_TOKEN` variable in the `.env` file (make sure you do not check this file into version control, as it contains your secret API token). 
-4. Ensure your computer has a camera, either a built in webcam or a USB camera. The application will automatically detect your camera. If you are on a Mac, the application might detect your iPhone as your primary webcam. See the notes in `main.py` for how to change this to your built in webcam, if desired.
+4. Ensure your computer has a camera, either a built in webcam or a USB camera. The application will automatically detect your camera. If you are on a Mac, the application might detect your iPhone as your primary webcam. See the notes in `setup.py` for how to change this to your built in webcam, if desired.
 5. Install python dependencies by running `pip install -r requirements.txt`.
 6. Aim your camera at a door.
 7. Run `python main.py` to start the application.
@@ -21,6 +21,7 @@ This example application uses Groundlight to answer the question: "Is the door o
 4. Ask us questions! We would love to help you get started. See how to contact us in the [Getting Started with Groundlight](#getting-started-with-groundlight) section above.
 
 ## [Optional] Deploy on Raspberry Pi
+If you have a Raspberry Pi with a camera, you can deploy this application on your Raspberry Pi. This is a great way to get started with Groundlight, as you can deploy your application in a low cost, low power environment.
 1. Flash your Raspberry Pi with the latest version of our Raspberry Pi Image. You can find instructions [here](https://github.com/groundlight/groundlight-pi-gen?tab=readme-ov-file#groundlight-pi-gen-os-images-for-raspberry-pi-with-groundlight-tools).
 2. Clone this repo on your Raspberry Pi.
 3. Follow the instructions above, in the [Your First Groundlight Application](#your-first-groundlight-application) section.
--- main.py
+++ main.py
@@ -2,53 +2,32 @@
 
 # This program will use Groundlight's Python SDK to answer a simple question: "Is the door open?"
 
-import os
 from time import sleep
 
-from dotenv import load_dotenv
-from framegrab import (
-    FrameGrabber,  # framegrab is our open-source Python library, which makes it easy to set up cameras for computer vision applications, like Groundlight.
-)
 from groundlight import Groundlight  # our Python SDK
 
-# loads the contents of the .env file into the environment, including your Groundlight API Token
-load_dotenv()
+from setup import load_and_validate_api_key, setup_camera
 
-# Check if GROUNDLIGHT_API_TOKEN is set in the .env file and starts with "api_", if not, raise an error.
-api_token = os.getenv("GROUNDLIGHT_API_TOKEN")
-if not api_token or not api_token.startswith("api_"):
-    raise ValueError(
-        "The 'GROUNDLIGHT_API_TOKEN' is not set correctly in the .env file. It should start with 'api_'. You can create a token here: https://app.groundlight.ai/reef/my-account/api-tokens."
-    )
+# load your Groundlight API Token from the .env file, and validate that it is set correctly.
+load_and_validate_api_key()
+
+# set up the camera using framegrab, our open-source Python library. You can learn more about it in setup.py.
+camera = setup_camera()
 
 # Initialize the SDK. We will use the gl object to interact with the Groundlight API.
 gl = Groundlight()
 
 # Question you want Groundlight to answer. Consider reading our short article on how to get the best results from Groundlight, which includes how to phrase your query: https://code.groundlight.ai/python-sdk/blog/best-practices
 query_text = "Is the door open? This includes if the door is only partially open."
-detector_name = "door_open_detector"
+
 # Create a Groundlight Detector. This is where we will send image queries for answers. You can see the performance of your detectors on our webapp: https://app.groundlight.ai
+detector_name = "door_open_detector"
+
 detector = gl.get_or_create_detector(
     name=detector_name,
     query=query_text,
 )
 
-# Next we have to set up the camera we will use for this application. Framegrab automatically detects all cameras connected to your computer, and this app uses the first one it finds (unless you change the camera_index variable below).
-# If you would like more sophisticated configuration of your camera, visit the framegrab repo for more information on how to automatically crop, zoom, set resolution, or add motion detection: https://github.com/groundlight/framegrab
-cameras = FrameGrabber.autodiscover()
-camera_index = 0  # NOTE: If on a Mac, framegrab might consider your iPhone as the first valid camera, so you might need to change this index to use your computer's webcam.
-
-if len(cameras) == 0:
-    raise ValueError("No cameras found. Please connect a camera and try again.")
-elif len(cameras) > 1:
-    print(f"Found {len(cameras)} cameras. Here are the configs for each camera:")
-    for camera in cameras.values():
-        print(camera.config)
-    print(
-        f'Using camera number {camera_index+1} from the list. If you would like to use a different camera, change the "camera_index" variable in main.py'
-    )
-
-camera = list(cameras.values())[camera_index]
 
 try:
     while True:
